b5eacf100d6cb48a66e531cef5fe464eff0757e3
AriasADevSchool <ariaadelheidschool@gmail.com>
2024-12-05 10:47:51 +0000

    Edited JS

--- script/index.js
+++ script/index.js
@@ -1,32 +1,48 @@
 const cursor = document.querySelector('.bg__gradient');
 
-if (cursor) {
-    let mouseX = 0;
-    let mouseY = 0;
+let mouseX = 0;
+let mouseY = 0;
 
-    let cursorX = 0;
-    let cursorY = 0;
+let cursorX = 0;
+let cursorY = 0;
 
-    const speed = 0.1;
+let speed = 0.01;
 
-    function animate() {
-        const distX = mouseX - cursorX;
-        const distY = mouseY - cursorY;
+function animate() {
+    let distX = mouseX - cursorX;
+    let distY = mouseY - cursorY;
 
-        cursorX += distX * speed;
-        cursorY += distY * speed;
+    cursorX = cursorX + (distX * speed);
+    cursorY = cursorY + (distY * speed);
 
-        cursor.style.transform = `translate(${cursorX - cursor.offsetWidth / 2}px, ${cursorY - cursor.offsetHeight / 2}px)`;
+    cursor.style.left = cursorX + 'px';
+    cursor.style.top = cursorY + 'px';
 
-        requestAnimationFrame(animate);
-    }
-
-    document.addEventListener('mousemove', (event) => {
-        mouseX = event.pageX;
-        mouseY = event.pageY;
-    });
-
-    animate();
-} else {
-    console.error('Element .bg__gradient not found!');
+    requestAnimationFrame(animate);
 }
+
+
+animate();
+
+document.addEventListener('mousemove', (event) => {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+})
+
+
+
+( function( $ ) {
+
+	"use strict";
+
+  $(".card").tilt({
+    maxTilt: 15,
+    perspective: 1500,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+    speed: 500,
+    glare: false,
+    maxGlare: 0.2,
+    scale: 1.01
+  });
+  
+}( jQuery ) );
